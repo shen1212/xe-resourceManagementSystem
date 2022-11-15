36d55be0aee98a3a92b32bf1a2579d321c1c6461
shen1212 <110951166+shen1212@users.noreply.github.com>
2022-11-15 15:58:20 +0800

    Add files via upload

---
+++ ResourceManagementAPI/src/main/java/com/rms/ResourceManagementAPI/ResourceManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.rms.ResourceManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagementApiApplication.class, args);
+	}
+
+}
