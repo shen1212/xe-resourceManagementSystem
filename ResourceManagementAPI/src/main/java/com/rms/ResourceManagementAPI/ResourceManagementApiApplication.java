0536382534025b8af83e0a5bc38fbb75e1ee8e6b
Hong Shen <hongshen1122@gmail.com>
2022-11-30 17:05:05 +0800

    Added new changes for dashboard

--- ResourceManagementAPI/src/main/java/com/rms/ResourceManagementAPI/ResourceManagementApiApplication.java
+++ ResourceManagementAPI/src/main/java/com/rms/ResourceManagementAPI/ResourceManagementApiApplication.java
@@ -1,9 +1,14 @@
 package com.rms.ResourceManagementAPI;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = { "com.rms.ResourceManagementAPI.controller","com.rms.ResourceManagementAPI.dao","com.rms.ResourceManagementAPI.model","com.rms.ResourceManagementAPI.Service"})
+
 public class ResourceManagementApiApplication {
 
 	public static void main(String[] args) {
